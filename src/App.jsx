35259b5bb71d01f1054846914f74bdc9af8c05d2
iamvysh <vyshnavthaithottathil@gmail.com>
2023-12-18 10:38:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+import './App.scss'
+import Navbar from './Components/navbar/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <section>
+
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+
+  
+    </>
+  )
+}
+
+export default App
